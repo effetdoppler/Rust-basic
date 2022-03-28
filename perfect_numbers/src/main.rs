225c38ba40750eb10a764f7853efa94b447f0fe1
Charles Zhang <charles.zhang@epita.fr>
2022-03-28 21:23:06 +0200

    implement perfect number

---
+++ perfect_numbers/src/main.rs
@@ -0,0 +1,42 @@
+use divisor_sum::divisor_sum;
+
+fn main()
+{
+    let mut i:u64 = 0;
+	while i < 100000
+	{
+		if is_perfect_number(i) == true
+		{
+			println!("{}", i);
+		}
+		i = i + 1;
+	}
+}
+
+fn is_perfect_number(n: u64) -> bool
+{
+    if divisor_sum(n) == n
+	{
+		true
+	}
+	else
+	{
+		false
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_is_perfect_number()
+    {
+        assert_eq!(is_perfect_number(6), true);
+        assert_eq!(is_perfect_number(12), false);
+        assert_eq!(is_perfect_number(28), true);
+        assert_eq!(is_perfect_number(165), false);
+        assert_eq!(is_perfect_number(496), true);
+    }
+}
